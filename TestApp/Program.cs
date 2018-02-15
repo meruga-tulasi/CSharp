969cf03e51f973067a39a78f306044f0d7dad7e2
Tulasiram Meruga <Tulasiram.Meruga@hotmail.com>
2018-02-15 17:10:43 -0500

    Project

---
+++ TestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, This is my first project in Git...");
+            Console.ReadKey();
+        }
+    }
+}
